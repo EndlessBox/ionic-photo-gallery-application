e91ca00d1d74efce5f53515047d02892b701583a
EndlessBox <ybouladh@1337.student.com>
2021-04-24 03:40:46 +0000

    add ios, android  deployement

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,98 +1,112 @@
-import { Injectable } from '@angular/core';
+import { Injectable, PlatformRef } from '@angular/core';
 import {
-  Plugins, CameraResultType, Capacitor, FilesystemDirectory,
-  CameraPhoto, CameraSource
+  Plugins,
+  CameraResultType,
+  Capacitor,
+  FilesystemDirectory,
+  CameraPhoto,
+  CameraSource,
 } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
 
 const { Camera, Filesystem, Storage } = Plugins;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PhotoService {
-
   public photos: Photo[] = [];
-  private PHOTO_STORAGE: string = "photos"
+  private PHOTO_STORAGE: string = 'photos';
+  private platform: Platform;
 
-
-  constructor() { }
-
+  constructor(platform: Platform) {
+    this.platform = platform;
+  }
 
   public async addNewToGallery() {
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
-      quality: 100
+      quality: 100,
     });
 
     const saveImageFile = await this.savePicture(capturedPhoto);
     this.photos.unshift(saveImageFile);
     Storage.set({
       key: this.PHOTO_STORAGE,
-      value: JSON.stringify(this.photos)
-    })
+      value: JSON.stringify(this.photos),
+    });
   }
 
-
   private async savePicture(cameraPhoto: CameraPhoto) {
-
     const base64 = await this.readAsBase64(cameraPhoto);
 
     const fileName = new Date().getTime() + '.jpeg';
     const savedFile = await Filesystem.writeFile({
       path: fileName,
       data: base64,
-      directory: FilesystemDirectory.Data
+      directory: FilesystemDirectory.Data,
     });
 
-    return ({
-      filePath: fileName,
-      webViewPath: cameraPhoto.webPath
-    })
-
+    if (this.platform.is('hybrid')) {
+      return {
+        filePath: savedFile.uri,
+        webViewPath: Capacitor.convertFileSrc(savedFile.uri),
+      };
+    } else {
+      return {
+        filePath: fileName,
+        webViewPath: cameraPhoto.webPath,
+      };
+    }
   }
-
 
   public async loadSaved() {
-    const photoList = await Storage.get({key: this.PHOTO_STORAGE});
-    this.photos = JSON.parse(photoList.value) || []
+    const photoList = await Storage.get({ key: this.PHOTO_STORAGE });
+    this.photos = JSON.parse(photoList.value) || [];
 
-    for (let photo of this.photos) {
-      const readFile = await Filesystem.readFile({
-        path: photo.filePath,
-        directory: FilesystemDirectory.Data
-      });
+    if (!this.platform.is('hybrid')) {
+      for (let photo of this.photos) {
+        const readFile = await Filesystem.readFile({
+          path: photo.filePath,
+          directory: FilesystemDirectory.Data,
+        });
 
-      photo.webViewPath = `data:image/jpeg;base64,${readFile.data}`
-    } 
+        photo.webViewPath = `data:image/jpeg;base64,${readFile.data}`;
+      }
+    }
   }
 
-
-
-  /* 
+  /*
    *  Helper Functions.
    */
   private async readAsBase64(cameraPhoto: CameraPhoto) {
-    const response = await fetch(cameraPhoto.webPath);
-    const blob = await response.blob();
+    if (this.platform.is('hybrid')) {
+      const file = await Filesystem.readFile({
+        path: cameraPhoto.path,
+      });
+      return file.data;
+    } else {
+      const response = await fetch(cameraPhoto.webPath);
+      const blob = await response.blob();
 
-    return await this.convertBlobToBase64(blob) as string;
+      return (await this.convertBlobToBase64(blob)) as string;
+    }
   }
 
   private convertBlobToBase64(blob: Blob) {
     return new Promise((resolve, reject) => {
-      const reader = new FileReader;
+      const reader = new FileReader();
 
       reader.onerror = reject;
 
       reader.onload = () => {
         resolve(reader.result);
-      }
-      reader.readAsDataURL(blob)
-    })
+      };
+      reader.readAsDataURL(blob);
+    });
   }
 }
-
 
 export interface Photo {
   filePath: string;
